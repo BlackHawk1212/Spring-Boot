9eabb356ea75eed991b25e8785d85de440d7892b
Blackhawk1212 <nimeshmendis1997@gmail.com>
2022-09-20 22:44:32 +0530

    Spring Boot Kafka Microservices video series

---
+++ Java_Guides/springboot-kafka-microservices/order-service/src/main/java/com/javaguides/orderservice/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.javaguides.orderservice.controller;
+
+import com.javaguides.basedomains.dto.Order;
+import com.javaguides.basedomains.dto.OrderEvent;
+import com.javaguides.orderservice.kafka.OrderProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer){
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order){
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("Order is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order placed successfully";
+    }
+
+}
